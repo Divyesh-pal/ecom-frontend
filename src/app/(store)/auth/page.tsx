350e94888106821f9ebd86c75f4c0d0007d6616b
Divyesh Pal <divyesh@Divyeshs-MacBook-Pro.local>
2026-04-29 18:13:52 +0530

    fix: remove unused Link import from auth page

--- src/app/(store)/auth/page.tsx
+++ src/app/(store)/auth/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
